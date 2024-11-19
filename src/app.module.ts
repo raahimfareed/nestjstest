a042421518fc39f21d90c5c43c552fa64d4fd35c
Raahim Fareed <raahimfareed@proton.me>
2024-11-19 15:44:04 +0500

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
+import { CatModule } from './cats/cat.module';
 import { LoggerMiddleware } from './logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './cats/cat.entity';
 
 @Module({
-  imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+        CatModule,
+        TypeOrmModule.forRoot({
+            type: 'mariadb',
+            host: 'localhost',
+            port: 3306,
+            username: 'nestjstest',
+            password: '12345678',
+            database: 'nestjstest',
+            entities: [Cat],
+            // Do not use synchronize in prod, can cause possible loss of data
+            synchronize: true
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
